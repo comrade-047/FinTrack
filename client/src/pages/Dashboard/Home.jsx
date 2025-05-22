efcc587e7d00a13d802d482e43258678d2a7751a
comrade-047 <amitkumar.4.7.09.27@gmail.com>
2025-05-22 14:18:10 +0530

    Dashboard Page Layout

--- client/src/pages/Dashboard/Home.jsx
+++ client/src/pages/Dashboard/Home.jsx
@@ -1,6 +1,16 @@
+import DashboardLayout from "../../components/Layout/DashboardLayout";
+import {useUserAuth} from "../../hooks/useUserAuth";
+
 const Home = () =>{
+
+    useUserAuth();
+
     return (
-        <div>Home</div>
+        <DashboardLayout activeMenu="Dashboard">
+            <div className="my-5 mx-auto">
+                
+            </div>
+        </DashboardLayout>
     )
 }
 export default Home;
